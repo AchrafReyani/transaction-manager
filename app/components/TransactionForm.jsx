bc292898970f6a035e7c17ce53f32727bfe3eac6
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-25 12:16:31 +0200

    added boilerplate html for the add transaction form

--- app/components/TransactionForm.jsx
+++ app/components/TransactionForm.jsx
@@ -1,3 +1,21 @@
 export default function TransactionForm() {
-    return <p>... transactions form ...</p>
+    return (
+        <form action="addTransaction">
+        <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+        </div>
+        <div>
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" name="description" required />
+        </div>
+        <div>
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" name="amount" required />
+        </div>
+        <button type="submit">
+            Add transaction
+        </button>
+    </form>
+    )
 }
