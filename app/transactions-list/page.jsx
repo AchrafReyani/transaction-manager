c80f40e5c8fe3a722496de3e8a4b4b6766d86e6c
Achraf Reyani <achrafreyani99@gmail.com>
2024-07-25 04:14:42 +0200

    add basic html for transactions page

--- app/transactions-list/page.jsx
+++ app/transactions-list/page.jsx
@@ -1,3 +1,19 @@
 export default function TransactionsList(){
-    return <h1>transactions list</h1>
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>
+                        Your Transactions
+                    </h1>
+                    <form action="/auth/signout" method="post">
+                        <button type="Submit">
+                            Sign out
+                        </button>
+                    </form>
+                </div>
+                <TransactionsList />
+            </div>
+        </div>
+    )
 }
